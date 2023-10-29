789f4d987653b3f0636e1a8fbabfcd71e46f1637
kjy93 <kjy.devops@gmail.com>
2023-10-29 21:38:06 +0900

    board-with-firebase

---
+++ board-with-firebase/src/App.js
@@ -0,0 +1,12 @@
+import { db } from "./firebase";
+
+function App() {
+  console.log(db);
+  return (
+    <div className="App">
+      <h1>Hello, React!!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ board-with-firebase/src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDu1fjryomjfIm9X88IoO3nabnkbJ-niz0",
+  authDomain: "test-project-f17b0.firebaseapp.com",
+  projectId: "test-project-f17b0",
+  storageBucket: "test-project-f17b0.appspot.com",
+  messagingSenderId: "583340958593",
+  appId: "1:583340958593:web:cefd0ce966359ac095be80",
+  measurementId: "G-2RETRH4RHT",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);
