05f99bd3732acd3c80fd17dc7fecc200bbd69d2e
kjy93 <kjy.devops@gmail.com>
2023-11-01 22:55:34 +0900

    파이어베이스 데이터 읽기 성공

--- board-with-firebase/src/App.js
+++ board-with-firebase/src/App.js
@@ -1,7 +1,24 @@
 import { db } from "./firebase";
+import { collection, getDocs, doc } from "firebase/firestore";
 
 function App() {
-  console.log(db);
+  // async function getProc(db) {
+  //   const proc = collection(db, "product");
+  //   const procSnapshot = await getDocs(proc);
+  //   const procList = procSnapshot.docs.map((doc) => doc.data());
+  //   console.log(procList);
+  // }
+  async function getProc() {
+    const querySnapshot = await getDocs(collection(db, "product"));
+    // console.log(querySnapshot);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data());
+    });
+    //   const docRef = doc(db, "product");
+    //   const docSnap = await getDoc(docRef);
+    //   console.log(docSnap.data());
+  }
+  getProc();
   return (
     <div className="App">
       <h1>Hello, React!!</h1>
