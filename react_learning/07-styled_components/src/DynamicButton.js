3c43c0bb7baf322da021d9cf60d3438d3bcd2636
TD-kim <kjy.devops@gmail.com>
2023-12-20 16:59:09 +0900

    20231220 styled components

---
+++ react_learning/07-styled_components/src/DynamicButton.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const SIZES = {
+  large: 24,
+  medium: 20,
+  small: 16,
+};
+
+const DynamicButton = styled.button`
+  background-color: #6750a4;
+  border: none;
+  /* border-radius: ${(props) => (props.round ? `9999px` : `3px`)}; */
+  border-radius: ${({ round }) => (round ? `9999px` : `3px`)};
+  color: #ffffff;
+  /* font-size: ${(props) => SIZES[props.size] ?? SIZES['medium']}px; */
+  font-size: ${({ size }) => SIZES[size] ?? SIZES['medium']}px;
+  padding: 16px;
+
+  &:hover,
+  &:active {
+    background-color: #463770;
+  }
+`;
+
+export default DynamicButton;
