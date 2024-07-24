3437da416702e07b8bb001a0494828bbd861f0cc
TD-Kim <kjy.devops@gmail.com>
2024-07-24 23:29:15 +0900

    Update ChatRoom.js

--- react_learning/11-realtime_chatting/src/components/ChatRoom.js
+++ react_learning/11-realtime_chatting/src/components/ChatRoom.js
@@ -1,9 +1,19 @@
 import {
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
   serverTimestamp,
 } from 'firebase/firestore';
 import React, { useEffect, useRef, useState } from 'react';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
-import { addDatas, db, getDatasOrderByLimit, getRealTimeMessages } from '../api/firebase';
+import {
+  addDatas,
+  db,
+  getDatasOrderByLimit,
+  getRealTimeMessages,
+} from '../api/firebase';
 import ChatMessage from './ChatMessage';
 import * as FaIcons from 'react-icons/fa';
 
@@ -16,17 +26,14 @@ function ChatRoom({ auth }) {
     const resultData = await getDatasOrderByLimit('messages', 'createdAt', 500);
     setMessages(resultData);
   };
-  console.log(messages)
   const handleLoad = async () => {
-    const [datas, unsubscribe] = await getRealTimeMessages('messages', 'createdAt', 500);
+    const [datas, unsubscribe] = await getRealTimeMessages(
+      'messages',
+      'createdAt',
+      500
+    );
     setMessages(datas);
-  }
-
-  // const messagesCollect = collection(db, 'messages');
-  // const q = query(messagesCollect, orderBy('createdAt'), limit(500));
-
-  // const [messages] = useCollectionData(q);
-  // console.log(messages);
+  };
 
   const sendMessage = async (e) => {
     e.preventDefault();
@@ -39,26 +46,40 @@ function ChatRoom({ auth }) {
     };
     await addDatas('messages', addObj);
     setFormValue('');
-    // dummy.current.scrollIntoView({ behavior: 'smooth' });
-    // getMessages();
-    // getMessages().then(() => {
-    //   dummy.current.scrollIntoView({ behavior: 'smooth' });
-    // });
   };
 
   useEffect(() => {
-    handleLoad()
+    // handleLoad()
+    let messages = [];
+    const q = query(
+      collection(db, 'messages'),
+      orderBy('createdAt'),
+      limit(500)
+    );
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const datas = [];
+      querySnapshot.forEach((doc) => {
+        datas.push(doc.data());
+      });
+      setMessages(datas);
+      dummy.current.scrollIntoView({ behavior: 'smooth' });
+    });
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+    return () => {
+      console.log('언제실행되냐?');
+      dummy.current.scrollIntoView({ behavior: 'smooth' });
+    };
   }, []);
 
   // useEffect(() => {
   //   getMessages();
   // }, []);
 
-  // useEffect(() => {
-  //   // scrollIntoView() 메소드는 자신이 호출된 요소가 사용자에게 표시되도록
-  //   // 상위 컨테이너를 스크롤한다.
-  //   dummy.current.scrollIntoView(false, { behavior: 'smooth' });
-  // }, [messages]);
+  useEffect(() => {
+    // scrollIntoView() 메소드는 자신이 호출된 요소가 사용자에게 표시되도록
+    // 상위 컨테이너를 스크롤한다.
+    dummy.current.scrollIntoView(false);
+  }, [messages]);
   return (
     <>
       <main>
