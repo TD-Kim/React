78cb46e2866c7bbf4c88b86f2176ff8601a9310b
TD-Kim <125251761+TD-Kim@users.noreply.github.com>
2024-08-08 17:29:03 +0900

    20240808

--- react_learning/16-shop-app/src/store/producs/productSlice.js
+++ react_learning/16-shop-app/src/store/producs/productSlice.js
@@ -5,12 +5,19 @@ import axios from 'axios';
 export const fetchProduct = createAsyncThunk(
   'product/fetchProduct',
   async (id, thunkAPI) => {
+    // dispatch: 다른 액션을 디스패치할 수 있게 해준다.
+    // getState: 현재 상태를 가져올 수 있게 해준다.
+    // extra: 미들웨어가 생성될 때 추가적으로 주입한 데이터를 포함한다.
+    // requestId: 이 비동기 요청의 고유 ID를 제공한ㄷ.
+    // signal: 비동기 요청을 취소하기 위한 AbortController의 signal을 포함한다.
+    // rejectWithValue: 사용자 정의 에러 메시지를 포함하여 리젝션을 처리할 수 있게 해준다.
     try {
       const response = await axios.get(
         `https://fakestoreapi.com/products/${id}`
       );
       return response.data;
     } catch (error) {
+      // 여기서 return 하는 값이 reducer 의 rejected 로 들어간다.
       return thunkAPI.rejectWithValue('Error loading product');
     }
   }
